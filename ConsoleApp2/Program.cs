214662aaaa314d6a2c24e0244fae86f399bad5d5
SamirKurbatov <samir.kurbatov.03@inbox.ru>
2023-10-31 20:54:15 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,25 @@
+﻿int number = int.Parse(Console.ReadLine());
+
+if (number % 3 == 0)
+{
+    var replacedStr = "fizz";
+    var str = StringExtension.Replace(number.ToString(), replacedStr);
+    Console.Write(str + " ");
+}
+
+
+class MyClass
+{
+
+}
+
+
+
+public static class StringExtension
+{
+    public static string Replace(this string inputStr, string replaceStr)
+    {
+        inputStr = replaceStr;
+        return inputStr;
+    }
+}
