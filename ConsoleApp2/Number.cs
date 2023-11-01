54c193f0efa12c402a7f17264588a0c7b8023535
SamirKurbatov <samir.kurbatov.03@inbox.ru>
2023-11-01 19:30:50 +0300

    CreateNumber

---
+++ ConsoleApp2/Number.cs
@@ -0,0 +1,15 @@
+﻿namespace TestTask
+{
+    public class Number
+    {
+        public string ReplacedWord { get; }
+
+        public int Reason { get; private set; }
+
+        public Number(string replacedWord, int reason)
+        {
+            ReplacedWord = replacedWord.ToLower();
+            Reason = reason;
+        }
+    }
+}
